f9ef15f77b3b08f3189885ec6da944615b6f0743
willian <mynamewra@gmail.com>
2023-05-11 02:03:12 -0300

    Login and Home page

--- template/js/home.js
+++ template/js/home.js
@@ -1,33 +1,9 @@
-const main = document.querySelector('main');
+const lista = document.querySelector('.lista');
+const item = document.querySelector('.item');
 
-const {data} = json;
-
-function renderPage(){
-    data.forEach(element => {
-        //Cria a seção que vai envolver a seção do conteúdo 
-        const section = createSection( element.title, 'h2');    
-        main.appendChild(section);
-        
-        const buttonRedirect = createButtonRedirect(element.linkAllCards,'button-redirect', 'bi bi-chevron-right')
-        const divWrapListCards = createListCards( buttonRedirect, 'd-flex  align-items-center', 'd-flex flex-row bd-highlight');
-        
-        section.appendChild(divWrapListCards)
-        
-        const ul = divWrapListCards.firstChild 
-        
-        
-        const quantityColumnsCards = getNumberCardsByWidthScreen()
-        const cards = element.cards.slice(0, quantityColumnsCards);
-        cards.forEach(card => {
-            const listItem = createCard(card.link, card.imgSrc, 'p-2');
-            ul.appendChild(listItem)
-        })
-    });
+for(let i=0; i<20;i++){
+  // Clona elemento html
+  const clone = item.cloneNode(true);
+  // Insere na lista o item clonado
+  lista.appendChild(clone);
 }
-    
-renderPage()
-
-window.addEventListener( 'resize' , event => {
-    main.innerHTML = '';
-    renderPage()
-}, )
