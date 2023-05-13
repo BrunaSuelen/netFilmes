fe973e8d95616fdaafd4b58c34f30a1d4a5e7a19
Bruna Suelen <subr.garcia@gmail.com>
2023-05-12 22:42:26 -0300

    feat: Adiciona mock

---
+++ db.js
@@ -0,0 +1,44 @@
+const db_serie = [
+	{
+		link: '/template/streaming.html',
+		title: 'Vikings ',
+		imgSrc: "images/streamings/netflix.png",
+    streaming: 'netflix',
+    categoria: 'desejo_assistir'
+	},
+	{
+		link: '/template/streaming.html',
+		title: 'Vikings ',
+    imgSrc: "images/streamings/netflix.png",
+    streaming: 'netflix',
+    categoria: 'desejo_assistir'
+	},
+	{
+		link: '/template/streaming.html',
+		title: 'Vikings ',
+    imgSrc: "images/streamings/netflix.png",
+    streaming: 'netflix',
+    categoria: 'desejo_assistir'
+	}
+]
+
+const db_streaming = [
+  {
+    id: '1',
+    title: 'Netflix ',
+    hash: 'netflix',
+    imgSrc: "images/streamings/netflix.png"
+  },
+  {
+    id: '2',
+    title: 'Globo Play ',
+    hash: 'globo-play',
+    imgSrc: "images/streamings/netflix.png"
+  },
+  {
+    id: '3',
+    title: 'HBO ',
+    hash: 'hbo',
+    imgSrc: "images/streamings/netflix.png"
+  }
+]
