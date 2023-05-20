a48a93d13b81edaceebb8d6abaa64fcc3f27af55
willian <mynamewra@gmail.com>
2023-05-19 21:27:59 -0300

    Botão desabilitado caso input não tenha valor

--- src/js/home.js
+++ src/js/home.js
@@ -1,14 +1,18 @@
-const lista = document.querySelector('.list-cards');
-const item = document.querySelector('.cards');
 
-//Insere na ul x quantidade o conteúdo dos cards 
-for(let i=0; i<20;i++){
-  // Clona elemento li e todos os elementos filhos
-  const clone = item.cloneNode(true);
-  // Insere na lista o li que clonado
-  lista.appendChild(clone);
+init();
+
+function init() {
+  const lista = document.querySelector('.list-cards');
+  const item = document.querySelector('.cards');
+
+  //Insere na ul x quantidade o conteúdo dos cards 
+  for (let i = 0; i < 20; i++) {
+    // Clona elemento li e todos os elementos filhos
+    const clone = item.cloneNode(true);
+    // Insere na lista o li que clonado
+    lista.appendChild(clone);
+  }
 }
-
 function editarSerie() {
   window.location.href = 'form-serie.html?editar=X';
 }
